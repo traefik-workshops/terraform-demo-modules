6568b9e685d7fd5526f91d68c6830f5b1726c0be
Zaid Albirawi <zalbiraw@gmail.com>
2025-05-20 21:36:31 -0400

    fix gke

--- clusters/gke/main.tf
+++ clusters/gke/main.tf
@@ -5,12 +5,7 @@ resource "google_container_cluster" "traefik_demo" {
   min_master_version  = var.gke_version
   location            = var.cluster_location
   deletion_protection = false
-
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count       = var.cluster_node_count
+  initial_node_count  = var.cluster_node_count
 
   monitoring_config {
     managed_prometheus {
