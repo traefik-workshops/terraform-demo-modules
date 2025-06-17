e38403f334561a9b56f647733e46e29510527542
Zaid Albirawi <zalbiraw@gmail.com>
2025-06-18 00:56:53 +0200

    decode lke cert

--- clusters/lke/outputs.tf
+++ clusters/lke/outputs.tf
@@ -1,6 +1,6 @@
 locals {
   kubeconfig_raw  = base64decode(linode_lke_cluster.traefik_demo.kubeconfig)
-  kubeconfig_json = jsondecode(local.kubeconfig_raw)
+  kubeconfig_json = yamldecode(local.kubeconfig_raw)
 
   cluster_entry   = local.kubeconfig_json.clusters[0].cluster
   cluster_server  = local.cluster_entry.server
@@ -8,11 +8,13 @@ locals {
 }
 
 output "host" {
+  sensitive   = true
   description = "LKE cluster host"
-  value = local.cluster_server
+  value       = local.cluster_server
 }
 
 output "cluster_ca_certificate" {
+  sensitive   = true
   description = "LKE cluster CA certificate"
-  value = local.cluster_ca_cert
+  value       = base64decode(local.cluster_ca_cert)
 }
