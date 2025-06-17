d8a6d516e3136b9ba2f7d61e50cf1418c6a09667
Zaid Albirawi <zalbiraw@gmail.com>
2025-06-18 00:22:21 +0200

    Add outputs

--- clusters/lke/outputs.tf
+++ clusters/lke/outputs.tf
@@ -1,9 +1,18 @@
-# output "host" {
-#   description = "LKE cluster host"
-#   value = linode_lke_cluster.traefik_demo.api_endpoints[0]
-# }
+locals {
+  kubeconfig_raw  = base64decode(linode_lke_cluster.traefik_demo.kubeconfig)
+  kubeconfig_json = jsondecode(local.kubeconfig_raw)
 
-# output "cluster_ca_certificate" {
-#   description = "LKE cluster CA certificate"
-#   value = base64decode(azurerm_kubernetes_cluster.traefik_demo.kube_config.0.cluster_ca_certificate)
-# }
+  cluster_entry   = local.kubeconfig_json.clusters[0].cluster
+  cluster_server  = local.cluster_entry.server
+  cluster_ca_cert = local.cluster_entry["certificate-authority-data"]
+}
+
+output "host" {
+  description = "LKE cluster host"
+  value = local.cluster_server
+}
+
+output "cluster_ca_certificate" {
+  description = "LKE cluster CA certificate"
+  value = local.cluster_ca_cert
+}
