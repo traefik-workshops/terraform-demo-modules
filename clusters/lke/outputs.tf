543ad236781fc883f07b15ce45aab67cbdcd95a1
Zaid Albirawi <zalbiraw@gmail.com>
2025-06-25 11:27:41 -0400

    Add kubeconfig to lke outputs

--- clusters/lke/outputs.tf
+++ clusters/lke/outputs.tf
@@ -1,5 +1,6 @@
 locals {
-  kubeconfig      = yamldecode(base64decode(linode_lke_cluster.traefik_demo.kubeconfig))
+  kubeconfig_raw  = base64decode(linode_lke_cluster.traefik_demo.kubeconfig)
+  kubeconfig      = yamldecode(local.kubeconfig_raw)
   cluster         = local.kubeconfig.clusters[0].cluster
   cluster_server  = local.cluster.server
   cluster_ca_cert = base64decode(local.cluster["certificate-authority-data"])
@@ -22,4 +23,10 @@ output "token" {
   sensitive   = true
   description = "LKE cluster auth token"
   value       = local.token
+}
+
+output "kubeconfig" {
+  sensitive   = true
+  description = "LKE cluster kubeconfig"
+  value       = local.kubeconfig_raw
 }
