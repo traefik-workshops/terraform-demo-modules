53534114eca4c8dc5673542254da0ed292e770cb
Zaid Albirawi <zalbiraw@gmail.com>
2025-07-17 13:58:25 -0400

    fix OKE

--- clusters/oke/main.tf
+++ clusters/oke/main.tf
@@ -17,7 +17,6 @@ data "oci_core_images" "traefik_demo" {
   sort_order               = "DESC"
 }
 
-# Create VCN
 resource "oci_core_vcn" "traefik_demo" {
   compartment_id = var.compartment_id
   display_name   = "${var.cluster_name}-vcn"
@@ -25,7 +24,6 @@ resource "oci_core_vcn" "traefik_demo" {
   dns_label      = "oke${replace(var.cluster_name, "-", "")}"
 }
 
-# Create Internet Gateway
 resource "oci_core_internet_gateway" "traefik_demo" {
   compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.traefik_demo.id
@@ -33,13 +31,6 @@ resource "oci_core_internet_gateway" "traefik_demo" {
   enabled        = true
 }
 
-resource "oci_core_nat_gateway" "traefik_demo" {
-  compartment_id = var.compartment_id
-  vcn_id         = oci_core_vcn.traefik_demo.id
-  display_name   = "${var.cluster_name}-natgw"
-}
-
-# Create Route Table
 resource "oci_core_route_table" "traefik_demo" {
   compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.traefik_demo.id
@@ -51,7 +42,6 @@ resource "oci_core_route_table" "traefik_demo" {
   }
 }
 
-# Create Security List
 resource "oci_core_security_list" "traefik_demo" {
   compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.traefik_demo.id
@@ -77,7 +67,6 @@ resource "oci_core_security_list" "traefik_demo" {
   }
 }
 
-# Create Endpoint Subnet
 resource "oci_core_subnet" "traefik_demo_endpoint" {
   compartment_id      = var.compartment_id
   vcn_id              = oci_core_vcn.traefik_demo.id
@@ -89,7 +78,6 @@ resource "oci_core_subnet" "traefik_demo_endpoint" {
   prohibit_public_ip_on_vnic = false
 }
 
-# Create Node Subnet
 resource "oci_core_subnet" "traefik_demo_nodes" {
   compartment_id      = var.compartment_id
   vcn_id              = oci_core_vcn.traefik_demo.id
@@ -101,7 +89,6 @@ resource "oci_core_subnet" "traefik_demo_nodes" {
   prohibit_public_ip_on_vnic = false
 }
 
-# Create Load Balancer Subnet
 resource "oci_core_subnet" "traefik_demo_lb" {
   compartment_id      = var.compartment_id
   vcn_id              = oci_core_vcn.traefik_demo.id
@@ -113,16 +100,11 @@ resource "oci_core_subnet" "traefik_demo_lb" {
   prohibit_public_ip_on_vnic = false
 }
 
-# Create OKE Cluster
 resource "oci_containerengine_cluster" "traefik_demo" {
   compartment_id     = var.compartment_id
   kubernetes_version = var.oke_version
   name               = var.cluster_name
   vcn_id             = oci_core_vcn.traefik_demo.id
-
-  cluster_pod_network_options {
-    cni_type = "OCI_VCN_IP_NATIVE"
-  }
 
   endpoint_config {
     is_public_ip_enabled = true
@@ -131,16 +113,16 @@ resource "oci_containerengine_cluster" "traefik_demo" {
 
   options {
     service_lb_subnet_ids = [oci_core_subnet.traefik_demo_lb.id]
-    
+
     add_ons {
       is_kubernetes_dashboard_enabled = false
       is_tiller_enabled               = false
     }
-    
+
     admission_controller_options {
       is_pod_security_policy_enabled = false
     }
-    
+
     kubernetes_network_config {
       pods_cidr     = "10.244.0.0/16"
       services_cidr = "10.96.0.0/16"
@@ -148,7 +130,6 @@ resource "oci_containerengine_cluster" "traefik_demo" {
   }
 }
 
-# Create Node Pool
 resource "oci_containerengine_node_pool" "traefik_demo" {
   cluster_id         = oci_containerengine_cluster.traefik_demo.id
   compartment_id     = var.compartment_id
@@ -157,10 +138,6 @@ resource "oci_containerengine_node_pool" "traefik_demo" {
   node_shape         = var.cluster_node_type
 
   node_config_details {
-    node_pool_pod_network_option_details {
-      cni_type = "OCI_VCN_IP_NATIVE"
-      pod_subnet_ids = [oci_core_subnet.traefik_demo_nodes.id]
-    } 
     placement_configs {
       availability_domain = data.oci_identity_availability_domains.traefik_demo.availability_domains[0].name
       subnet_id           = oci_core_subnet.traefik_demo_nodes.id
@@ -191,7 +168,7 @@ data "oci_containerengine_cluster_kube_config" "kubeconfig" {
 
 data "external" "cluster_token" {
   depends_on = [oci_containerengine_node_pool.traefik_demo]
-  
+
   program = ["bash", "-c", <<-EOT
     token_response=$(oci ce cluster generate-token --cluster-id ${oci_containerengine_cluster.traefik_demo.id} --region ${var.cluster_location})
     token=$(echo "$token_response" | awk -F'"' '/"token":/ {print $4}')
