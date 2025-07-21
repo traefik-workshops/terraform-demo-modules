6faf5471ac99f277977010c918099237cdba0e88
Zaid Albirawi <zalbiraw@gmail.com>
2025-07-21 08:15:37 -0400

    add oke node_pool_id output

--- clusters/oke/outputs.tf
+++ clusters/oke/outputs.tf
@@ -36,3 +36,9 @@ output "cluster_id" {
   description = "OKE cluster ID"
   value       = oci_containerengine_cluster.traefik_demo.id
 }
+
+output "node_pool_id" {
+  sensitive   = true
+  description = "OKE node pool ID"
+  value       = oci_containerengine_node_pool.traefik_demo.id
+}
