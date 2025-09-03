4aa7c6739847303972f9a56823248868a86f35d3
Zaid Albirawi <zalbiraw@gmail.com>
2025-09-03 00:13:08 -0400

    fix lke

--- clusters/lke/main.tf
+++ clusters/lke/main.tf
@@ -35,14 +35,14 @@ resource "null_resource" "lke_cluster" {
   provisioner "local-exec" {
     
     command = <<EOT
-      echo '${module.lke.kubeconfig}' > lke-kubeconfig.yaml
+      echo '${local.kubeconfig_raw}' > lke-kubeconfig.yaml
 
       export KUBECONFIG=~/.kube/config:lke-kubeconfig.yaml
       kubectl config view --flatten > merged.yaml
       mv merged.yaml ~/.kube/config
 
       kubectl config delete-context "lke-${var.cluster_name}" 2>/dev/null || true
-      kubectl config rename-context "lke${module.lke.cluster_id}-ctx" "lke-${var.cluster_name}"
+      kubectl config rename-context "lke${linode_lke_cluster.traefik_demo.id}-ctx" "lke-${var.cluster_name}"
       kubectl config use-context "lke-${var.cluster_name}"
 
       rm lke-kubeconfig.yaml
