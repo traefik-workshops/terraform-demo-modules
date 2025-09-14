8b1e537ec15eaba8ca9abb09dd22272c4803d6c9
Zaid Albirawi <zalbiraw@gmail.com>
2025-09-14 17:09:29 +0300

    Update k3d setup

--- clusters/k3d/main.tf
+++ clusters/k3d/main.tf
@@ -6,22 +6,30 @@ apiVersion: k3d.io/v1alpha5
 kind: Simple
 metadata:
   name: ${var.cluster_name}
-servers: 1
+servers: ${var.control_plane_nodes.count}
+agents: ${sum([for node in var.worker_nodes : node.count])}
 ports:
-  - port: 8000:80
+%{ for port in var.ports ~}
+  - port: ${port.to}:${port.from}
     nodeFilters:
       - loadbalancer
-  - port: 8443:443
-    nodeFilters:
-      - loadbalancer
-  - port: 8080:8080
-    nodeFilters:
-      - loadbalancer
+%{ endfor ~}
 options:
   k3s:
     extraArgs:
       - arg: "--disable=traefik"
         nodeFilters:
           - "server:*"
+          - "agent:*"
+    nodeLabels:
+%{ for node_idx, node in var.worker_nodes ~}
+%{ if node.label != "" ~}
+%{ for instance in range(0, node.count) ~}
+      - label: ${node.label}
+        nodeFilters:
+          - agent:${sum([for i in range(0, node_idx): var.worker_nodes[i].count]) + instance}
+%{ endfor ~}
+%{ endif ~}
+%{ endfor ~}
 EOF
 }
--- clusters/k3d/variables.tf
+++ clusters/k3d/variables.tf
@@ -1,4 +1,33 @@
 variable "cluster_name" {
   type        = string
-  description = "k3d cluster name"
+  description = "k3d cluster name."
+}
+
+variable "control_plane_nodes" {
+  type = object({
+    count = number
+  })
+  default     = { count: 1 }
+  description = "Cluster Control Plane node config."
+}
+
+variable "worker_nodes" {
+  type = list(object({
+    label = string
+    count = number
+  }))
+  default = []
+  description = "Worker node config."
+}
+
+variable "ports" {
+  type = list(object({
+    from = number
+    to   = number
+  }))
+  default = [
+    { from: 80,   to: 8000},
+    { from: 8443, to: 443},
+    { from: 8080, to: 8080},
+  ]
 }
