5c4a6a143e6b67f3525b01772958a71268339103
Zaid Albirawi <zalbiraw@gmail.com>
2025-09-24 15:10:44 +0200

    add preview tag support

---
+++ k8s/grafana/main.tf
@@ -0,0 +1,61 @@
+locals {
+  prometheus_url = var.prometheus.url.override != "" ? var.prometheus.url.override : "http://${var.prometheus.url.service}${var.prometheus.url.namespace != "" ? ".${var.prometheus.url.namespace}.svc" : ""}:${var.prometheus.url.port}"
+  tempo_url      = var.tempo.url.override      != "" ? var.tempo.url.override      : "http://${var.tempo.url.service}${var.tempo.url.namespace != "" ? ".${var.tempo.url.namespace}.svc" : ""}:${var.tempo.url.port}"
+  loki_url       = var.loki.url.override       != "" ? var.loki.url.override       : "http://${var.loki.url.service}${var.loki.url.namespace != "" ? ".${var.loki.url.namespace}.svc" : ""}:${var.loki.url.port}"
+
+  datasources = concat(
+    var.prometheus.enabled ? [{
+      name = "Prometheus"
+      type = "prometheus"
+      url = local.prometheus_url
+      access = "proxy"
+      isDefault = true
+    }] : [],
+    var.tempo.enabled ? [{
+      name = "Tempo"
+      type = "tempo"
+      url = local.tempo_url
+      access = "proxy"
+      isDefault = ! var.prometheus.enabled
+    }] : [],
+    var.loki.enabled ? [{
+      name = "Loki"
+      type = "loki"
+      url = local.loki_url
+      access = "proxy"
+      isDefault = ! var.prometheus.enabled && ! var.tempo.enabled
+    }] : [])
+}
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  namespace  = var.namespace
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  version    = "10.0.0"
+  timeout    = 900
+  atomic     = true
+
+  values = [
+    yamlencode({
+      "grafana.ini" = {
+        "auth.anonymous" = {
+          enabled = true
+          org_name = "Main Org."
+          org_role = "Admin"
+        }
+        auth = {
+          disable_login_form = true
+        }
+      }
+      datasources = {
+        "datasources.yaml" = {
+          apiVersion = 1
+          datasources = local.datasources
+        }
+      }
+      tolerations = var.tolerations
+    }),
+    yamlencode(var.extraValues)
+  ]
+}
---
+++ k8s/grafana/variables.tf
@@ -0,0 +1,60 @@
+variable "namespace" {
+  type        = string
+  description = "Namespace for the Grafana deployment"
+}
+
+variable "prometheus" {
+  type = object({
+    enabled = bool
+    url     = object({
+      override  = string
+      service   = string
+      port      = number
+      namespace = string
+    })
+  })
+  description = "Prometheus datasource configuration."
+}
+
+variable "tempo" {
+  type = object({
+    enabled = bool
+    url     = object({
+      override  = string
+      service   = string
+      port      = number
+      namespace = string
+    })
+  })
+  description = "Tempo datasource configuration."
+}
+
+variable "loki" {
+  type = object({
+    enabled = bool
+    url     = object({
+      override  = string
+      service   = string
+      port      = number
+      namespace = string
+    })
+  })
+  description = "Loki datasource configuration."
+}
+
+variable "tolerations" {
+  type = list(object({
+    key      = string
+    operator = string
+    value    = string
+    effect   = string
+  }))
+  description = "Tolerations for the Grafana deployment."
+  default     = []
+}
+
+variable "extraValues" {
+  type = any
+  description = "Extra values to pass to the Grafana deployment."
+  default     = {}
+}
