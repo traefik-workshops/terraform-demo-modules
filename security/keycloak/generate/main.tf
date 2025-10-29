2f3dd607b5b89d80e7f77dcabb406057981e24ce
Zaid Albirawi <zalbiraw@gmail.com>
2025-10-29 17:32:56 -0400

    add token generation for keycloak

---
+++ security/keycloak/generate/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Generate JWT tokens for each user
+data "http" "keycloak_token" {
+  for_each = { for idx, user in var.users : idx => user }
+
+  url    = "${var.keycloak_url}/realms/${var.realm}/protocol/openid-connect/token"
+  method = "POST"
+
+  request_headers = {
+    Content-Type = "application/x-www-form-urlencoded"
+  }
+
+  request_body = join("&", [
+    "client_id=${urlencode(var.client_id)}",
+    "grant_type=password",
+    "client_secret=${urlencode(var.client_secret)}",
+    "scope=openid",
+    "username=${urlencode(each.value.username)}",
+    "password=${urlencode(each.value.password)}"
+  ])
+
+  lifecycle {
+    postcondition {
+      condition     = self.status_code == 200
+      error_message = "Failed to obtain JWT token for user ${each.value.username}: HTTP ${self.status_code}"
+    }
+  }
+}
+
+locals {
+  # Parse the JWT tokens from the responses
+  tokens = {
+    for idx, response in data.http.keycloak_token :
+    var.users[idx].username => jsondecode(response.response_body).access_token
+  }
+}
---
+++ security/keycloak/generate/variables.tf
@@ -0,0 +1,30 @@
+variable "keycloak_url" {
+  description = "Keycloak base URL (e.g., https://keycloak.example.com)"
+  type        = string
+}
+
+variable "realm" {
+  description = "Keycloak realm name"
+  type        = string
+  default     = "traefik"
+}
+
+variable "client_id" {
+  description = "Keycloak client ID"
+  type        = string
+  default     = "traefik"
+}
+
+variable "client_secret" {
+  description = "Keycloak client secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "users" {
+  description = "List of users to generate JWT tokens for"
+  type = list(object({
+    username = string
+    password = string
+  }))
+}
