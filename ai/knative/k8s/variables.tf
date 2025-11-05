4a9d57783ed1d29cd664600bc459e007d94727ea
immanuelfodor <21174107+immanuelfodor@users.noreply.github.com>
2025-11-05 13:35:28 +0100

    Add knative ingress domain, fix namespace

--- ai/knative/k8s/variables.tf
+++ ai/knative/k8s/variables.tf
@@ -8,4 +8,9 @@ variable "namespace" {
   type        = string
   description = "The namespace of the knative release"
 }
-  
+
+variable "ingress_domain" {
+  type        = string
+  description = "The external domain where knative will publish services. Eg. knative.kubeata.traefikhub.dev will result in <service>.<namespace>.knative.kubeata.traefikhub.dev ingresses"
+  default     = "knative.kubeata.traefikhub.dev"
+}
