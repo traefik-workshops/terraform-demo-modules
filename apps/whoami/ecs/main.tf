1cb970a3fcbac2ead780b7cd10f2eee94ad6ec07
Zaid Albirawi <zalbiraw@gmail.com>
2025-11-05 12:11:07 -0500

    update aws config

--- apps/whoami/ecs/main.tf
+++ apps/whoami/ecs/main.tf
@@ -7,9 +7,7 @@ locals {
           for app_name, app_config in cluster_config.apps : app_name => merge(
             app_config,
             {
-              docker_image       = "traefik/whoami:latest"
-              subnet_ids         = try(coalesce(app_config.subnet_ids, cluster_config.subnet_ids, var.subnet_ids), var.subnet_ids)
-              security_group_ids = try(coalesce(app_config.security_group_ids, cluster_config.security_group_ids, []), [])
+              docker_image = "traefik/whoami:latest"
             }
           )
         }
