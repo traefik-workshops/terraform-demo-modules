5e58d06e630388b30fa30b6ae9349f6c2d0cca2c
Zaid Albirawi <zalbiraw@gmail.com>
2025-12-27 11:29:08 -0500

    update nutanix output

--- compute/nutanix/nkp/outputs.tf
+++ compute/nutanix/nkp/outputs.tf
@@ -1,5 +1,10 @@
+output "control_plane_fip" {
+  description = "Control plane FIP"
+  value       = local.control_plane_fip
+}
+
 output "kubeconfig" {
-  description = "Kubeconfig content for the cluster (with FIP address)"
+  description = "Kubeconfig content for the cluster"
   value       = try(data.local_file.kubeconfig.content, "")
   sensitive   = true
 }
--- traefik/k8s/crds.tf
+++ traefik/k8s/crds.tf
@@ -1,5 +1,7 @@
 
 resource "helm_release" "traefik-crds" {
+  count = var.skip_crds ? 0 : 1
+
   name       = "traefik-crds"
   namespace  = var.namespace
   repository = "https://traefik.github.io/charts"
@@ -7,24 +9,26 @@ resource "helm_release" "traefik-crds" {
   version    = "1.11.1"
   timeout    = 900
   atomic     = true
-  
+
   set = [
     {
-      name = "gatewayAPI"
-      value = true
-    },
-    { 
-      name = "hub"
+      name  = "gatewayAPI"
       value = true
     },
     {
-      name = "deleteOnUninstall"
+      name  = "hub"
+      value = true
+    },
+    {
+      name  = "deleteOnUninstall"
       value = true
     }
   ]
 }
 
 resource "null_resource" "gateway_api_experimental" {
+  count = var.skip_crds ? 0 : 1
+
   provisioner "local-exec" {
     command = <<EOF
     kubectl apply --server-side \
