59d28a6158bad12599bb42f8b65a0234b066ccec
Zaid Albirawi <zalbiraw@gmail.com>
2026-01-20 08:17:39 -0500

    add more options to traefik nutanix deployment

--- traefik/nutanix/image_builder/main.tf
+++ traefik/nutanix/image_builder/main.tf
@@ -3,13 +3,14 @@ locals {
 }
 
 resource "terraform_data" "build_image" {
+  count            = var.image_path == null ? 1 : 0
   triggers_replace = [var.arch]
 
   provisioner "local-exec" {
     command = <<EOT
       cd ${path.module}/packer
       if [ ! -f images/${local.image_filename} ]; then
-        make packer-build-${var.arch} HUB_DIR="${var.hub_dir}"
+        make packer-build-${var.arch} HUB_DIR="${var.hub_dir != null ? var.hub_dir : ""}" HUB_VERSION="${var.hub_version}"
       fi
     EOT
   }
@@ -17,7 +18,7 @@ resource "terraform_data" "build_image" {
 
 resource "nutanix_image" "traefik_hub" {
   name        = "traefik-hub_${var.arch}.qcow2"
-  source_path = "${path.module}/packer/images/${local.image_filename}"
+  source_path = var.image_path != null ? var.image_path : "${path.module}/packer/images/${local.image_filename}"
   description = "Traefik Hub Pre-packaged Image"
 
   depends_on = [terraform_data.build_image]
--- traefik/nutanix/image_builder/variables.tf
+++ traefik/nutanix/image_builder/variables.tf
@@ -7,5 +7,17 @@ variable "arch" {
 variable "hub_dir" {
   description = "Path to the traefik-hub repository"
   type        = string
-  default     = "../../../../../traefik-hub"
+  default     = null
+}
+
+variable "hub_version" {
+  description = "Traefik Hub version to build from"
+  type        = string
+  default     = "3.18.3"
+}
+
+variable "image_path" {
+  description = "Optional path to a pre-existing image file (skips building but still uploads)"
+  type        = string
+  default     = null
 }
