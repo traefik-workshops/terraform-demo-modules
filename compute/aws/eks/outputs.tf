9a9f2d6136ac42849b4a288469cface39713a906
Zaid Albirawi <zalbiraw@gmail.com>
2026-01-27 15:42:14 -0500

    expose aws node group security group

--- compute/aws/eks/outputs.tf
+++ compute/aws/eks/outputs.tf
@@ -23,3 +23,8 @@ output "cluster_security_group_id" {
   description = "Security group ID attached to the EKS cluster"
   value       = module.eks.cluster_security_group_id
 }
+
+output "node_security_group_id" {
+  description = "Security group ID attached to the EKS worker nodes"
+  value       = module.eks.node_security_group_id
+}
