2dff276c7dee4572efafc27e247fcb614b5e79e7
Zaid Albirawi <zalbiraw@gmail.com>
2026-02-04 16:47:31 -0500

    Update nutanix fips setup to allow for vm and ip based with clashes and state resets

--- compute/nutanix/fip/main.tf
+++ compute/nutanix/fip/main.tf
@@ -1,25 +1,44 @@
-resource "nutanix_floating_ip_v2" "fip" {
+resource "nutanix_floating_ip_v2" "vm_fip" {
+  count = var.vm_nic_uuid != "" ? 1 : 0
+
   name                      = var.name
   external_subnet_reference = var.external_subnet_uuid
 
   association {
-    dynamic "vm_nic_association" {
-      for_each = var.vm_nic_uuid != "" ? [1] : []
-      content {
-        vm_nic_reference = var.vm_nic_uuid
-      }
+    vm_nic_association {
+      vm_nic_reference = var.vm_nic_uuid
     }
+  }
 
-    dynamic "private_ip_association" {
-      for_each = var.private_ip != "" && var.vpc_uuid != "" ? [1] : []
-      content {
-        vpc_reference = var.vpc_uuid
-        private_ip {
-          ipv4 {
-            value = var.private_ip
-          }
+  lifecycle {
+    precondition {
+      condition     = var.private_ip == "" && var.vpc_uuid == ""
+      error_message = "Conflicting configuration: vm_nic_uuid cannot be used with private_ip or vpc_uuid."
+    }
+  }
+}
+
+resource "nutanix_floating_ip_v2" "vpc_fip" {
+  count = var.private_ip != "" && var.vpc_uuid != "" ? 1 : 0
+
+  name                      = var.name
+  external_subnet_reference = var.external_subnet_uuid
+
+  association {
+    private_ip_association {
+      vpc_reference = var.vpc_uuid
+      private_ip {
+        ipv4 {
+          value = var.private_ip
         }
       }
+    }
+  }
+
+  lifecycle {
+    precondition {
+      condition     = var.vm_nic_uuid == ""
+      error_message = "Conflicting configuration: private_ip/vpc_uuid cannot be used with vm_nic_uuid."
     }
   }
 }
