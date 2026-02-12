b2b9fdee0dd5129b37558c9407f1b5cb073663cb
Zaid Albirawi <zalbiraw@gmail.com>
2026-02-12 12:52:30 -0500

    remove resources

--- resources/airlines/k8s/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "airlines_domain" {
-  value       = var.unique_domain ? try(data.kubernetes_secret.domain_secret[0].data["domain"], "Use 'terraform refresh' to fetch value") : "airlines.${var.domain}"
-  description = "The domain of the airlines application"
-}
